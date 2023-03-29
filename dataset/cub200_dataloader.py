f7667fe3583b484e3676bc28622157f16ef7e399
micha <michal.wronka00@gmail.com>
2023-03-29 17:10:27 +0200

    vit trainer from Dino that also saves logits, dataset updates

--- dataset/cub200_dataloader.py
+++ dataset/cub200_dataloader.py
@@ -1,57 +1,80 @@
 import os
+
+import numpy as np
 import torch
 from PIL import Image
+
+
 # https://pytorch.org/tutorials/beginner/data_loading_tutorial.html
-class CUB200DataLoader(torch.utils.data.Dataset):
+class CUB200Dataset(torch.utils.data.Dataset):
     # cutouts - if dataloader is to load cutouts or images
     # train - loads train or test set
-    def __init__(self,path,transform=None,cutouts=True,train=True):
+    # subset - limits classes to only the selected ones, None or False for all classes
+    def __init__(self, path, transform=None, cutouts=True, train=True,
+                 subset=[3, 5, 9, 15, 17, 18, 20, 21, 27, 29, 36, 44, 45, 46, 47, 51, 64, 72, 82, 84, 87, 90, 91, 92,
+                         93, 98, 99, 100, 104, 106, 107, 108, 110, 111, 134, 139, 141, 149, 173, 187, 199, 200]
+                 ):
         self.train = train
         self.cutouts = cutouts
         self.path = path
         self.transform = transform
-        self.imgs_ids = {}  # name -> id
-        self.imgs_names = {}  # id -> name
-        self.imgs_path = {}
-        with open(os.path.join(path,'images.txt')) as file:
-            for row in file:
-                row = row.split(' ')
-                # the '-1' removes trailing \n
-                id, name = int(row[0]), row[1][row[1].find('/') + 1:]
-                self.imgs_path[id] = row[1][:-1]
-                self.imgs_ids[name] = id
-                self.imgs_names[id] = name
-        self.train_ids = {} # idx -> id
-        self.test_ids = {} # idx -> id
-        with open(os.path.join(path,'train_test_split.txt')) as file:
-            for row in file:
-                row = row.split(' ')
-                id, is_train = int(row[0]), (int(row[1]) == 1)
-                if is_train:
-                    self.train_ids[len(self.train_ids)] = id
-                else:
-                    self.test_ids[len(self.train_ids)] = id
-
-        self.imgs_class = {}  # name -> class
-        with open(os.path.join(path,'image_class_labels.txt')) as file:
+        self.subset = subset
+        if subset:
+            self.mapping_cl_to_idx = {cl:i for i,cl in enumerate(subset)}
+            self.mapping_idx_to_cl = {i:cl for i,cl in enumerate(subset)}
+        self.imgs_class = {}  # id -> class
+        with open(os.path.join(path, 'image_class_labels.txt')) as file:
             for row in file:
                 row = row.split(' ')
                 id, cls = int(row[0]), int(row[1])
                 self.imgs_class[id] = cls
+        self.imgs_ids = {}  # name -> id
+        self.imgs_names = {}  # id -> name
+        self.imgs_path = {}
+        with open(os.path.join(path, 'images.txt')) as file:
+            for row in file:
+                row = row.split(' ')
+                # the '-1' removes trailing \n
+                id, name = int(row[0]), row[1][row[1].find('/') + 1:]
+                if subset and self.imgs_class[id] in subset:
+                    self.imgs_path[id] = row[1][:-1]
+                    self.imgs_ids[name] = id
+                    self.imgs_names[id] = name
+        self.train_ids = {}  # idx -> id
+        self.test_ids = {}  # idx -> id
+        with open(os.path.join(path, 'train_test_split.txt')) as file:
+            for row in file:
+                row = row.split(' ')
+                id, is_train = int(row[0]), (int(row[1]) == 1)
+                if subset and self.imgs_class[id] in subset:
+                    if is_train:
+                        self.train_ids[len(self.train_ids)] = id
+                    else:
+                        self.test_ids[len(self.test_ids)] = id
+
     def __len__(self):
         if self.train:
             return len(self.train_ids)
         else:
             return len(self.test_ids)
+
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        id = ( self.train_ids[idx] if self.train else self.test_ids[idx])
+        id = (self.train_ids[idx] if self.train else self.test_ids[idx])
         if self.cutouts:
-            img = Image.open(os.path.join(self.path,'cutouts',self.imgs_path[id]))
+            img = Image.open(os.path.join(self.path, 'cutouts', self.imgs_path[id]))
         else:
-            img = Image.open(os.path.join(self.path,'images',self.imgs_path[id]))
+            img = Image.open(os.path.join(self.path, 'images', self.imgs_path[id]))
         if self.transform:
             img = self.transform(img)
-        return (img,self.imgs_class[id])
+        label = self.imgs_class[id] if self.subset is None else self.mapping_cl_to_idx[self.imgs_class[id]]
+        return img, label
+    def get_filename(self,idx):
+        id = (self.train_ids[idx] if self.train else self.test_ids[idx])
+        return self.imgs_path[id]
+    def get_id(self,idx):
+        id = (self.train_ids[idx] if self.train else self.test_ids[idx])
+        return id
+
---
+++ dataset/vits_finetune.py
@@ -0,0 +1,142 @@
+from typing import Dict, Any
+
+import torch
+from transformers import ViTFeatureExtractor, ViTModel
+from PIL import Image
+import requests
+import numpy as np
+from cub200_dataloader import CUB200Dataset
+from torch.utils.data import DataLoader
+from torchvision.transforms import transforms
+import pytorch_lightning as pl
+from transformers import AdamW
+import torch.nn as nn
+from pytorch_lightning import Trainer
+import os
+from pytorch_lightning.callbacks import EarlyStopping
+
+bs = 64
+feature_extractor = ViTFeatureExtractor.from_pretrained('facebook/dino-vits8')
+trans = transforms.Compose([
+    transforms.Lambda(lambda x: feature_extractor(images=x, return_tensors="pt"))
+])
+ds_train = CUB200Dataset('./caltech_birds2011/CUB_200_2011', train=True, transform=trans)
+ds_test = CUB200Dataset('./caltech_birds2011/CUB_200_2011', train=False, transform=trans)
+dl_train = DataLoader(ds_train, batch_size=bs, shuffle=True)
+dl_test = DataLoader(ds_test, batch_size=bs, shuffle=False)
+
+
+class ViTLightningModule(pl.LightningModule):
+    def __init__(self, num_labels=42):
+        super(ViTLightningModule, self).__init__()
+        self.vit = ViTModel.from_pretrained('facebook/dino-vits8').cuda()
+        self.final = nn.Linear(384, num_labels).cuda()
+        self.unfreeze()
+        self.on_save_checkpoint(None)
+
+    def forward(self, pixel_values):
+        outputs = self.vit(pixel_values=pixel_values)
+        last_hidden_states = outputs.last_hidden_state
+        cls = last_hidden_states[:, 0, :]
+        outputs = self.final(cls)
+        return outputs
+
+    def freeze(self) -> None:
+        for name, layer in self.vit.named_modules():
+            for param in layer.parameters():
+                param.requires_grad = False
+        for param in self.final.parameters():
+            param.requires_grad = False
+
+    def unfreeze(self) -> None:
+        for name, layer in self.vit.named_modules():
+            for param in layer.parameters():
+                param.requires_grad = True if name in ['pooler.dense', 'pooler', 'pooler.activation'] else False
+        for param in self.final.parameters():
+            param.requires_grad = True
+
+    def common_step(self, batch, batch_idx):
+        imgs, labels = batch
+        labels = labels.cuda()
+        imgs = imgs['pixel_values'].cuda().squeeze()
+        logits = self(imgs)
+
+        criterion = nn.CrossEntropyLoss()
+        predictions = logits.argmax(-1)
+        loss = criterion(logits, labels)
+        accuracy = torch.where(predictions == labels, 1.0, 0.0)
+        accuracy = torch.mean(accuracy)
+
+        return loss, accuracy
+
+    def training_step(self, batch, batch_idx):
+        loss, accuracy = self.common_step(batch, batch_idx)
+        # logs metrics for each training_step,
+        # and the average across the epoch
+        self.log("training_loss", loss)
+        self.log("training_accuracy", accuracy)
+
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        loss, accuracy = self.common_step(batch, batch_idx)
+        self.log("test_loss", loss, on_epoch=True)
+        self.log("test_accuracy", accuracy, on_epoch=True)
+
+        return loss
+
+    def configure_optimizers(self):
+        # We could make the optimizer more fancy by adding a scheduler and specifying which parameters do
+        # not require weight_decay but just using AdamW out-of-the-box works fine
+        return AdamW(self.parameters(), lr=5e-5)
+
+    def train_dataloader(self):
+        return dl_train
+
+    def test_dataloader(self):
+        return dl_test
+
+    def on_save_checkpoint(self, checkpoint: Dict[str, Any]) -> None:
+        self.freeze()
+        self.eval()
+        with torch.no_grad():
+            path = os.path.join('./caltech_birds2011/CUB_200_2011/logits')
+            for ds, ds_name in zip([ds_train, ds_test], ['train', 'test']):
+                all = 0
+                correct = 0
+                for idx in range(len(ds)):
+                    # ds has __len__, but not __next__ or __iter__ so iterating over it is not allowed
+                    imgs, label = ds[idx]
+                    imgs = imgs['pixel_values'].cuda()
+                    outs = self(imgs)
+                    name = str(ds.get_id(idx)) + '.pt'
+                    torch.save(outs, os.path.join(path, name))
+                    prediction = outs.argmax(-1).cpu().item()
+                    correct += 1 if prediction == label else 0
+                    all += 1
+                print("Acc on {} = {:.5}".format(ds_name, correct / all))
+        self.train()
+        self.unfreeze()
+        for name, params in self.vit.named_parameters():
+            if name in ['pooler.dense.bias','pooler.dense.weight']:
+                checkpoint['vit_head_'+name] = params
+    def on_load_checkpoint(self, checkpoint: Dict[str, Any]) -> None:
+        for name, params in self.vit.named_parameters():
+            if name in ['pooler.dense.bias','pooler.dense.weight']:
+                params = checkpoint['vit_head_'+name]
+
+
+
+# for early stopping, see https://pytorch-lightning.readthedocs.io/en/1.0.0/early_stopping.html?highlight=early%20stopping
+early_stop_callback = EarlyStopping(
+    monitor='training_accuracy',
+    patience=5,
+    strict=False,
+    verbose=False,
+    mode='min'
+)
+
+model = ViTLightningModule()
+trainer = Trainer(devices=1, callbacks=[early_stop_callback], log_every_n_steps=5)
+trainer.fit(model)
+model.save_checkpoint() # also saves logits
